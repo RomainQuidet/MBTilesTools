cbb5524980e4331604ce31c0632d66ec864c6cc2
Romain Quidet <contact@xdappfactory.com>
2017-04-27 18:35:30 +0200

    adding gitignore, reader unit tests

---
+++ MBTilesToolsTests/MBTilesReaderTests.m
@@ -0,0 +1,33 @@
+//
+//  MBTilesReaderTests.m
+//  MBTilesReaderTests
+//
+//  Created by Romain Quidet on 27/04/2017.
+//  Copyright © 2017 XDAppfactory. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MBTilesReader.h"
+
+@interface MBTilesReaderTests : XCTestCase
+
+@end
+
+@implementation MBTilesReaderTests
+
+- (void)setUp {
+    [super setUp];
+
+}
+
+- (void)tearDown {
+
+    [super tearDown];
+}
+
+- (void)testExample {
+
+}
+
+
+@end
