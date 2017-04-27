bb5a69d9de37bbb7f05e01f7b42ba7fcb899485a
Romain Quidet <contact@xdappfactory.com>
2017-04-27 18:20:36 +0200

    Initial Commit

---
+++ MBTilesTools/MBTilesTools.h
@@ -0,0 +1,19 @@
+//
+//  MBTilesTools.h
+//  MBTilesTools
+//
+//  Created by Romain Quidet on 27/04/2017.
+//  Copyright © 2017 XDAppfactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBTilesTools.
+FOUNDATION_EXPORT double MBTilesToolsVersionNumber;
+
+//! Project version string for MBTilesTools.
+FOUNDATION_EXPORT const unsigned char MBTilesToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBTilesTools/PublicHeader.h>
+
+
